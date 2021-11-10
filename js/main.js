a6cfed55d498b60ecb2b0c6b4593b62de5c93d10
mohammedsobhi70 <sobhi7070@gmail.com>
2021-11-10 17:25:01 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// calculate discount
+let oldPrice = document.querySelectorAll(".old-price span");
+let newPrice = document.querySelectorAll(".new-price span");
+let discount = document.querySelectorAll(".discount");
+for (let i = 0; i < oldPrice.length; i++) {
+  discount[i].innerHTML =
+    "- " +
+    (
+      ((+oldPrice[i].innerHTML - newPrice[i].innerHTML) /
+        +oldPrice[i].innerHTML) *
+      100
+    ).toFixed(2) +
+    "%";
+}
+// end discount
+// scrolltop
+let scrolBtn = document.querySelector(".scrol-top");
+window.onscroll = function () {
+  if (window.scrollY > 300) {
+    scrolBtn.style.display = "inline-block";
+    document.querySelector(".navbar").classList.add("fixed-top");
+    document.querySelector(".navbar").classList.add("bg-white");
+  } else {
+    scrolBtn.style.display = "none";
+    document.querySelector(".navbar").classList.remove("fixed-top");
+    document.querySelector(".navbar").classList.remove("bg-white");
+  }
+};
+
+console.log(document.querySelector(".navbar").classList);
+scrolBtn.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+// end scrol
+let productDetails = document.querySelectorAll("button.product-details");
+productDetails.forEach((it) => {
+  it.addEventListener("click", function () {
+    location.href = "../pages/productdetails.html";
+  });
+});
